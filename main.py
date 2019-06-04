b4da5c893566d5125efd631f2e407220f8cf587a
aryan2906 <43298145+aryan2906@users.noreply.github.com>
2019-06-04 21:49:02 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,6 +9,23 @@ import youtube_dl
 import wikipedia
 import random
 import pyaudio
+import pyttsx3
+import datetime
+from twilio.rest import Client
+engine=pyttsx3.init()
+engine.say("Welcome sir")
+engine.runAndWait()
+# account_sid = 'AC7504911dbd847700d6f86a438d75a99f'
+# auth_token = '241329502f99b4b9772279fad0b6a807'
+# client = Client(account_sid, auth_token)
+#
+# message = client.messages.create(
+#                               from_='whatsapp:+14155238886',
+#                               body='Your Yummy Cupcakes Company order of 1 dozen frosted cupcakes has shipped and should be delivered on July 10, 2019. Details: http://www.yummycupcakes.com/',
+#                               to='whatsapp:+917906224093'
+#                           )
+
+print(message.sid)
 def myCommand():
     r = sr.Recognizer()
     with sr.Microphone() as source:
@@ -24,6 +41,9 @@ def myCommand():
         print('....')
         command = myCommand();
     return command
+def response(audio):
+    engine.say(audio)
+    engine.runAndWait()
 def assistant(command):
     print("I am here")
     if "open" in command:
@@ -34,6 +54,12 @@ def assistant(command):
         print("Redirecting to",url)
         webbrowser.open(url)
         print("done")
+        response(mi+" has been opened for you sir")
+    elif 'time' in command:
+        now=datetime.datetime.now()
+        response('Current time is %d hours %d minutes'%(now.hour,now.minute))
+    else:
+        response('This function is currently beyond my ability but I will soon learn it')
 
 
 
